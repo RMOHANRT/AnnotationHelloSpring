caa06ca590f3ca81de83db78a11df50165bcd735
RMOHANRT <rmohanr.t@gmail.com>
2017-11-30 01:45:58 +0530

    Initial commit

---
+++ src/com/ram/anno/spring/SpellChecker.java
@@ -0,0 +1,10 @@
+package com.ram.anno.spring;
+
+public class SpellChecker {
+	   public SpellChecker(){
+	      System.out.println("Inside SpellChecker constructor." );
+	   }
+	   public void checkSpelling(){
+	      System.out.println("Inside checkSpelling." );
+	   }
+	}
